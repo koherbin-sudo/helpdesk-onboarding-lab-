fe0f78b8becff474bb1c98b850404c3d511bd54d
koherbin-sudo <koherbin@gmail.com>
2026-03-03 12:36:48 -0500

    Update reset-password.ps1

--- scripts/m365/reset-password.ps1
+++ scripts/m365/reset-password.ps1
@@ -1,1 +1,11 @@
+Import-Module MSOnline
+
+Connect-MsolService
+
+$user = "jdoe@company.com"
+$newPassword = "TempP@ssw0rd123"
+
+Set-MsolUserPassword -UserPrincipalName $user -NewPassword $newPassword -ForceChangePassword $true
+
+Write-Host "Password reset for $user. User must change password at next login."
 
